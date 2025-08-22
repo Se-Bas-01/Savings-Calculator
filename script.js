a5d7f1570ccd8e72f8369835be29f97eba83d1fa
Sebastien King <sebastienking0511@gmail.com>
2025-08-22 11:21:17 +1000

    added function to javaScript and mod html.

---
+++ script.js
@@ -0,0 +1,15 @@
+function calculateSum() {
+    const goal = parseFloat(document.getElementById('goal').value);
+    const current = parseFloat(document.getElementById('currentAmount').value);
+    const weekly = parseFloat(document.getElementById('weeklyRate').value);
+
+    if(isNaN(goal) || isNaN(weekly) || isNaN(current)) {
+        alert('Please enter a valid number!');
+        return;
+    };
+
+    const amtLeft = goal - current;
+    const weekLeft = amtLeft / weekly;
+
+    document.getElementById('monLeft').textContent = amtLeft;
+}
